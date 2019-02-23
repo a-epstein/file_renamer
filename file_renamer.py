a35fd84aa8149865a8711d820204694dca4ee1ee
a-epstein <38995679+a-epstein@users.noreply.github.com>
2019-02-23 06:31:59 -0500

    Update file_renamer.py
    Added an English version of the file renamer and began to rewrite it in Python

--- file_renamer.py
+++ file_renamer.py
@@ -1,14 +1,39 @@
-"""Docstring"""
+"""File renamer: In this path, modify names of files after user navigates to a path and runs renamer.py"""
 
 # Libraries
 import os
 
-# Assign list of all files in directory to nameList
-nameList = os.listdir()
-print(nameList)
+# Get the current path
+path = os.getcwd()
 
-# String you want to add
-newName = "cool"
+# Display current path to user
+print(path)
+
+# User input
+# newFileName = Ask the user for the new file name (a naming pattern? prefix, suffix, etc)
+newFileName = input("Enter the new file name: ")
+
+# Display a confirmation message to the user that files will be renamed to newFileName
+print("Files will be renamed to: " + newFileName)
+
+"""
+# Walk through path and rename all files to newFileName, ignoring the last 4 characters in each name string
+If item in path is a file
+ext = get last 4 characters in name
+rename item with the new file name + ext
+repeat for all items in path
 
 #  Rename files
 os.rename(file + newName for file in nameList)
+
+# Inform the user that files have been renamed
+Print "Files have been renamed"
+"""
+
+
+
+
+
+
+
+
