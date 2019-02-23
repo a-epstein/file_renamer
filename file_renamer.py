70e482c79bebf8a7fbeab598aad2d4e914596897
a-epstein <38995679+a-epstein@users.noreply.github.com>
2019-02-23 12:47:51 -0500

    Splits file extension off filename
    Saves the file extension to a variable ext so it can be reconcatenated onto the new file name

--- file_renamer.py
+++ file_renamer.py
@@ -9,17 +9,29 @@ path = os.getcwd()
 # Display current path to user
 print(path)
 
-# User input
 # newFileName = Ask the user for the new file name (a naming pattern? prefix, suffix, etc)
 newFileName = input("Enter the new file name: ")
 
 # Display a confirmation message to the user that files will be renamed to newFileName
 print("Files will be renamed to: " + newFileName)
 
+# Walk through all files in directory
+# Ignore subfolders !!!
+nameList = os.listdir()
+for file in nameList: 
+    # Ignore hidden files
+    if not file.startswith('.'):
+        print(file)
+        # Find file extension and add to newFileName
+        ext = file.split('.')
+        ext = '.' + ext[-1]
+        rename = newFileName + ext
+        print(rename)
+
 """
-# Walk through path and rename all files to newFileName, ignoring the last 4 characters in each name string
+# Walk through path and rename all files to newFileName, ignoring the file extension in each name string
 If item in path is a file
-ext = get last 4 characters in name
+ext = get last n characters in name up to and including '.'
 rename item with the new file name + ext
 repeat for all items in path
 
@@ -29,11 +41,3 @@ os.rename(file + newName for file in nameList)
 # Inform the user that files have been renamed
 Print "Files have been renamed"
 """
-
-
-
-
-
-
-
-
